da3c185a01880d70a3a3597ca41fd27de5128912
baldenka <109846977+baldenka@users.noreply.github.com>
2022-07-28 20:39:57 +0500

    Delete prog3.py

--- prog3.py
+++
@@ -1,9 +0,0 @@
-def f(x,y):
-    if x==y:
-        return 1
-    if x>y or x == 2300:
-        return 0
-    else:
-        return f(x+100, y)+f(x+200, y)+f(x+300, y)
-print(f(800, 3500)*f(3500, 4200))
-
